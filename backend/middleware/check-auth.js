205173dbc9e0ad13172451a786acd87a1b4d7dc0
unknown <folarniyi@gmail.com>
2025-06-02 16:10:53 +0100

    start

---
+++ backend/middleware/check-auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+
+const HttpError = require("../models/http-error");
+
+require("dotenv").config();
+
+const JWT_KEY = process.env.JWT_KEY;
+
+module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
+    if (!token) {
+      throw new Error("Authentication failed!");
+    }
+    const decodedToken = jwt.verify(token, JWT_KEY);
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (err) {
+    const error = new HttpError("Authentication failed!", 403);
+    return next(error);
+  }
+};
---
+++ frontend/src/places/pages/NewPlace.jsx
@@ -0,0 +1,106 @@
+import React, { useContext } from 'react';
+
+
+import Input from '../../shared/components/FormElements/Input';
+import Button from '../../shared/components/FormElements/Button';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ImageUpload from '../../shared/components/FormElements/ImageUpload';
+import {
+  VALIDATOR_REQUIRE,
+  VALIDATOR_MINLENGTH
+} from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
+import { useHttpClient } from '../../shared/hooks/http-hook';
+import { AuthContext } from '../../shared/context/auth-context';
+import './PlaceForm.css';
+import { useNavigate } from 'react-router';
+
+const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
+const VITE_ASSET_URL = import.meta.env.VITE_ASSET_URL;
+const NewPlace = () => {
+  const auth = useContext(AuthContext);
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [formState, inputHandler] = useForm(
+    {
+      title: {
+        value: '',
+        isValid: false
+      },
+      description: {
+        value: '',
+        isValid: false
+      },
+      address: {
+        value: '',
+        isValid: false
+      },
+      image: {
+        value: null,
+        isValid: false
+      }
+    },
+    false
+  );
+
+  const navigate = useNavigate();
+
+  const placeSubmitHandler = async event => {
+    event.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append('title', formState.inputs.title.value);
+      formData.append('description', formState.inputs.description.value);
+      formData.append('address', formState.inputs.address.value);
+      formData.append('image', formState.inputs.image.value);
+      await sendRequest(VITE_BACKEND_URL + 'places', 'POST', formData, {
+        Authorization: 'Bearer ' + auth.token
+      });
+      navigate("/");
+    } catch (err) {}
+  };
+
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      <form className="place-form" onSubmit={placeSubmitHandler}>
+        {isLoading && <LoadingSpinner asOverlay />}
+        <Input
+          id="title"
+          element="input"
+          type="text"
+          label="Title"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid title."
+          onInput={inputHandler}
+        />
+        <Input
+          id="description"
+          element="textarea"
+          label="Description"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          errorText="Please enter a valid description (at least 5 characters)."
+          onInput={inputHandler}
+        />
+        <Input
+          id="address"
+          element="input"
+          label="Address"
+          validators={[VALIDATOR_REQUIRE()]}
+          errorText="Please enter a valid address."
+          onInput={inputHandler}
+        />
+        <ImageUpload
+          id="image"
+          onInput={inputHandler}
+          errorText="Please provide an image."
+        />
+        <Button type="submit" disabled={!formState.isValid}>
+          ADD PLACE
+        </Button>
+      </form>
+    </React.Fragment>
+  );
+};
+
+export default NewPlace;
